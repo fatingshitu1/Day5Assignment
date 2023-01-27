fa64043cd790bceec692393c850aa16e59e1d778
shitalfating <shitalfating84@gmail.com>
2023-01-27 15:29:10 +0530

    Refactored:Checking vowel or not

--- src/com/bridgelabz/BasicCore/VowelOrConsonant.java
+++ src/com/bridgelabz/BasicCore/VowelOrConsonant.java
@@ -1,7 +1,22 @@
 package com.bridgelabz.BasicCore;
 
+import java.util.Scanner;
+
 public class VowelOrConsonant {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Char");
+        char ch = scanner.next().charAt(0);
+        switch (ch){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch +  "Vowel");
+                break;
+            default:
+                System.out.println("This is a Consonant");
+        }
     }
 }
